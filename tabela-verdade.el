6a6c2b47f3df60d686062b5e68a0e998b8c24662
renkel <renkel@debian-renkel>
2025-08-17 01:20:32 -0300

    Video - Corte 7 - completo

--- tabela-verdade.el
+++ tabela-verdade.el
@@ -36,6 +36,66 @@
 ;; (mapconcat (lambda (x) (number-to-string (expt 2 x))) '(0 1 2 3 4) "|")
 ;; "1|2|4|8|16"
 
+;; --------------------------------------
+
+;; AND bit-a-bit  (AND logico): `logand'
+
+;; 10 = 1010 -> | 1 | 0 | 1 | 0 ||   |
+;; (logand 1 2)
+
+;; 0001 & 0000 = 0
+;; (logand 1 0)
+;; 0001 & 0001 = 1
+;; (logand 1 1)
+;; 0001 & 0010 = 0
+;; (logand 1 2)
+;; 0001 & 0011 = 1
+;; (logand 1 3)
+
+;; --------------------------------------
+;; Deslocamento (shift) aritmetico: `ash'
+
+;; (dotimes (x 4)
+;;   (insert "\n" (number-to-string (ash 1 x))))
+
+;; (progn
+;;   (insert "\n\n")
+;;   (dotimes (x 8)
+;;     (dotimes (y 3)
+;;       (let ((rev (- 3  y 1)))
+;;         (insert (number-to-string rev) (if (= (logand x (ash 1 rev)) 0) ": 0 | " ": 1 | "))))
+;;     (insert "\n")))
+
+;; 2: 0 | 1: 0 | 0: 0 |
+;; 2: 0 | 1: 0 | 0: 1 |
+;; 2: 0 | 1: 1 | 0: 0 |
+;; 2: 0 | 1: 1 | 0: 1 |
+;; 2: 1 | 1: 0 | 0: 0 |
+;; 2: 1 | 1: 0 | 0: 1 |
+;; 2: 1 | 1: 1 | 0: 0 |
+;; 2: 1 | 1: 1 | 0: 1 |
+
+;; --------------------------------------
+
+;; Loop `dolist'
+
+;; (progn
+;;   (insert "\n\n")
+;;   (dotimes (x 8)
+;;     (dolist (y (number-sequence 2 0 -1))
+;;       (insert (if (= (logand x (ash 1 y)) 0) " 0 " " 1 ")))
+;;     (insert "\n")))
+
+
+;; 0  0  0
+;; 0  0  1
+;; 0  1  0
+;; 0  1  1
+;; 1  0  0
+;; 1  0  1
+;; 1  1  0
+;; 1  1  1
+
 
 (defun demo/insert-truth-table ()
   "Insert uma tabela de verdade no ponto do cursor"
@@ -54,9 +114,22 @@
     ;; Atualiza o ponto de retorno ao fim da impressao.
     (setq start-pos (+ (point) (* vars 4) 4))
     ;; Imprimir as linhas da tabela.
-
+    (dotimes (i nrows)
+      (insert " |")
+      (dolist (j (number-sequence (- vars 1) 0 -1))
+        (insert (if (= (logand i (ash 1 j)) 0) " 0 |" " 1 |")))
+      (insert "|   |\n"))
     ;; Volta a celula da saida da primera expressao.
-
-    ))
+    (goto-char start-pos)))
 
 (provide 'demo/insert-truth-table)
+;; | A | B | C || S |
+;; +---+---+---++---+
+;; | 0 | 0 | 0 ||   |
+;; | 0 | 0 | 1 ||   |
+;; | 0 | 1 | 0 ||   |
+;; | 0 | 1 | 1 ||   |
+;; | 1 | 0 | 0 ||   |
+;; | 1 | 0 | 1 ||   |
+;; | 1 | 1 | 0 ||   |
+;; | 1 | 1 | 1 ||   |
